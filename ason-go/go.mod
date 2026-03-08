4571c9d68f03fdd58a5628b700ab5f6bf37b3b1e
Athan <athanseal@gmail.com>
2026-03-08 19:39:10 +1300

    chore: remove embedded projects before submodule migration

--- ason-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ason-lab/ason-go
-
-go 1.24.0
--- ason-java/src/main/java/io/ason/AsonException.java
+++
@@ -1,6 +0,0 @@
-package io.ason;
-
-public class AsonException extends RuntimeException {
-    public AsonException(String message) { super(message); }
-    public AsonException(String message, Throwable cause) { super(message, cause); }
-}
